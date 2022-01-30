6846cc5614eea952e54c82581da52c57157de8e0
Yash8016 <95992264+Yash8016@users.noreply.github.com>
2022-01-30 14:06:44 +0530

    Update __telegraph.py

--- Yumeko/modules/__telegraph.py
+++ Yumeko/modules/__telegraph.py
@@ -1,27 +1,84 @@
-from pyrogram import filters
-from pyrogram.types import Message
+from Yumeko.events import register
+from Yumeko import telethn as tbot
+TMP_DOWNLOAD_DIRECTORY = "./"
+from telethon import events
+import os
+from PIL import Image
+from datetime import datetime
+from telegraph import Telegraph, upload_file, exceptions
+Sammy = "Yumeko"
+telegraph = Telegraph()
+r = telegraph.create_account(short_name=Sammy)
+auth_url = r["auth_url"]
 
-from Yumeko import app, telegraph
-from Yumeko.core.decorators.errors import capture_err
 
-__MODULE__ = "Telegraph"
-__HELP__ = "/telegraph [Page name]: Paste styled text on telegraph."
+@register(pattern="^/t(m|xt) ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    optional_title = event.pattern_match.group(2)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        r_message = await event.get_reply_message()
+        input_str = event.pattern_match.group(1)
+        if input_str == "m":
+            downloaded_file_name = await tbot.download_media(
+                r_message,
+                TMP_DOWNLOAD_DIRECTORY
+            )
+            end = datetime.now()
+            ms = (end - start).seconds
+            h = await event.reply("Downloaded to {} in {} seconds.".format(downloaded_file_name, ms))
+            if downloaded_file_name.endswith((".webp")):
+                resize_image(downloaded_file_name)
+            try:
+                start = datetime.now()
+                media_urls = upload_file(downloaded_file_name)
+            except exceptions.TelegraphException as exc:
+                await h.edit("ERROR: " + str(exc))
+                os.remove(downloaded_file_name)
+            else:
+                end = datetime.now()
+                ms_two = (end - start).seconds
+                os.remove(downloaded_file_name)
+                await h.edit("Uploaded to https://telegra.ph{})".format(media_urls[0]), link_preview=True)
+        elif input_str == "xt":
+            user_object = await tbot.get_entity(r_message.sender_id)
+            title_of_page = user_object.first_name # + " " + user_object.last_name
+            # apparently, all Users do not have last_name field
+            if optional_title:
+                title_of_page = optional_title
+            page_content = r_message.message
+            if r_message.media:
+                if page_content != "":
+                    title_of_page = page_content
+                downloaded_file_name = await tbot.download_media(
+                    r_message,
+                    TMP_DOWNLOAD_DIRECTORY
+                )
+                m_list = None
+                with open(downloaded_file_name, "rb") as fd:
+                    m_list = fd.readlines()
+                for m in m_list:
+                    page_content += m.decode("UTF-8") + "\n"
+                os.remove(downloaded_file_name)
+            page_content = page_content.replace("\n", "<br>")
+            response = telegraph.create_page(
+                title_of_page,
+                html_content=page_content
+            )
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.reply("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
+    else:
+        await event.reply("Reply to a message to get a permanent telegra.ph link.")
 
 
-@app.on_message(filters.command("telegraph"))
-@capture_err
-async def paste(_, message: Message):
-    reply = message.reply_to_message
+def resize_image(image):
+    im = Image.open(image)
+    im.save(image, "PNG")
 
-    if not reply or not reply.text:
-        return await message.reply("Reply to a text message")
 
-    if len(message.command) < 2:
-        return await message.reply("**Usage:**\n /tgm [Page name]")
 
-    page_name = message.text.split(None, 1)[1]
-    page = telegraph.create_page(page_name, html_content=(reply.text.html).replace("\n", "<br>"))
-    return await message.reply(
-        f"**Posted:** {page['url']}",
-        disable_web_page_preview=True,
-    )
+
+__mod_name__ = "Tɢʀᴀᴘʜ"
